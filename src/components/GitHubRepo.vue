cf7feb40e7945c8ce2df34bd222dcc6db5e9a239
Ekaterina Vostrikova <ekavos@hotmail.com>
2025-07-23 23:14:19 +0200

    feat: add translation, elaborate repos table

--- src/components/GitHubRepo.vue
+++ src/components/GitHubRepo.vue
@@ -1,14 +1,47 @@
 <template>
-  <ul class="list-group">
-    <li class="list-group-item d-flex justify-content-between align-items-start">
-      <div class="ms-2 me-auto">
-        <a :href="urlRepo" target="_blank" :title="description" class="p-3">{{ repoName }}</a>
-      </div>
-      <span class="badge bg-primary rounded-pill">{{ forksCount }}</span>
-    </li>
-  </ul>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">{{ $t('reposTableRepos') }}</th>
+        <th scope="col">{{ $t('reposTableUpdate') }}</th>
+        <th scope="col">{{ $t('reposTableLang') }}</th>
+        <th scope="col">gh-pages</th>
+        <th scope="col">forks</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="repo in repos" :key="repo.id">
+        <td>
+          <a
+            :href="repo.html_url"
+            target="_blank"
+            rel="noopener noreferrer"
+            :title="repo.description"
+            >{{ repo.name }}</a
+          >
+        </td>
+        <td>{{ formattedDate(repo) }}</td>
+        <td>{{ repo.language }}</td>
+        <td>
+          <a :href="ghPagesLink(repo)" target="_blank" rel="noopener noreferrer">{{
+            ghPagesLink(repo)
+          }}</a>
+        </td>
+        <td>{{ repo.forks_count }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
-defineProps(['repoName', 'urlRepo', 'description', 'forksCount']);
+defineProps(['repos']);
+
+const formattedDate = (repo) => {
+  const date = new Date(Date.parse(repo.updated_at));
+  return date.toLocaleDateString();
+};
+
+const ghPagesLink = (repo) => {
+  return repo.has_pages ? `https://${repo.owner.login}.github.io/${repo.name}` : '-';
+};
 </script>
